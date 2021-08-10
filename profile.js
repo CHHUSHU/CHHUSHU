1d35a1cebc8efa5087f3caf07f6b904ad8ab5e01
Chushu <39640111+CHHUSHU@users.noreply.github.com>
2021-08-10 16:54:48 +0800

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,21 @@
+{
+    "first_name": "Chushu",
+    "last_name": "Tan",
+    "Summary":""
+    "positions":[
+    {"job_title": "Informatics and Data Engineer", "organization": "Xian Sai Biotech", "duration": ""},
+    {"job_title": "Bioinformatics Engineer", "organization": "San Ji Biotech", "duration": ""}
+    ],
+    "education": [
+    {"school_name": "China University of Political Science and Law", "program": "Master in Applied Psychology", "duration":"2021-"},
+    {"school_name": "Monash University", "program": "Master of IT", "duration":"2018-2019"},
+    {"school_name": "State University of New York at Buffalo", "program": "BA in Psychology", "duration":"2015-2017"},
+    {"school_name": "State University of New York at Buffalo", "program": "BS in Biological Science", "duration":"2011-2015"}
+    ],
+    "contact_info":[
+    "phone_no": 18008428969,
+    "email": "375814935@qq.com",
+    "github": "CHHUSHU"
+    ]
+
+}
